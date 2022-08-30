5b0c9f4724dfd7b173b7e9cf70eea5411c891952
Robert Gleason <l2ob122@gmail.com>
2022-08-29 21:21:23 -0700

    Created POJO and Added Logic into main

--- src/com/coderscampus/assignment3/UserLoginApplication.java
+++ src/com/coderscampus/assignment3/UserLoginApplication.java
@@ -1,10 +1,38 @@
 package com.coderscampus.assignment3;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class UserLoginApplication {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		UserService userService = new UserService();
+		User[] userArray = new User[4];
 
+		Scanner scan = new Scanner(System.in);
+
+		BufferedReader fileReader = null;
+
+		try {
+			fileReader = new BufferedReader(new FileReader("data.txt"));
+
+			String line;
+			int i = 0;
+			while ((line = fileReader.readLine()) != null) {
+				String[] userData = line.split(",");
+				User userInfo = new User(userData[0], userData[1], userData[2]);
+				userArray[i] = userInfo;
+				i++;
+				System.out.println(line);
+			}
+
+		} finally {
+
+			fileReader.close();
+
+		}
 	}
 
 }
---
+++ src/com/coderscampus/assignment3/UserService.java
@@ -0,0 +1,15 @@
+package com.coderscampus.assignment3;
+
+public class UserService {
+
+	public User createUser(String username, String password, String name) {
+
+		User user = new User(username, password, name);
+
+		user.setUsername(username);
+		user.setPassword(password);
+		user.setName(name);
+
+		return user;
+	}
+}
