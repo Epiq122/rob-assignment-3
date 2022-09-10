2e8372ad24ed8522c4191cb6166c3e3d315a74a7
Robert Gleason <l2ob122@gmail.com>
2022-09-10 08:13:06 -0700

    fixed bug where program doesnt end after 5 login attempts, added a attempts remaning counter

--- src/com/coderscampus/assignment3/User.java
+++ src/com/coderscampus/assignment3/User.java
@@ -2,20 +2,30 @@ package com.coderscampus.assignment3;
 
 public class User {
 
+
+
+	// This allows us to output the username,password and name into the console and show all the information
 	public String toString() {
 		return "UserInfo [username =  " + username + ", password = " + password + ", name = " + name + "]";
 	}
 
+
+
+	//  This creates a user constructor used to apply the username , namr and password using this
 	public User(String username, String password, String name) {
 		this.username = username;
 		this.password = password;
 		this.name = name;
 	}
 
+
+	// These are our variables
 	private String username;
 	private String password;
 	private String name;
 
+
+	// these are the getters and setters
 	public String getUsername() {
 		return username;
 	}
--- src/com/coderscampus/assignment3/UserLoginApplication.java
+++ src/com/coderscampus/assignment3/UserLoginApplication.java
@@ -1,5 +1,8 @@
 package com.coderscampus.assignment3;
 
+
+// all the required imports from out rpject
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -9,18 +12,29 @@ public class UserLoginApplication {
 
 	public static void main(String[] args) throws IOException {
 
+		// Creates an array and adds 5 indexes into it
 		User[] userArray = new User[5];
 		try (Scanner scan = new Scanner(System.in)) {
 			BufferedReader fileReader = null;
 
+
+			// our try block for the the file reader
 			try {
 				fileReader = new BufferedReader(new FileReader("data.txt"));
 
+
 				String line;
 				int i = 0;
+
+				// assigned line to the file reader
 				while ((line = fileReader.readLine()) != null) {
+
+					// Create a user data Array and take our file reader  we called line and add the split method so we can add ,
 					String[] userData = line.split(",");
+
+					// create a userInfo and add 3 sections of data we had in our user class (username,password,name)
 					User userInfo = new User(userData[0], userData[1], userData[2]);
+					//create a userArray and assing it to the user info
 					userArray[i] = userInfo;
 					i++;
 					System.out.println(userInfo);
@@ -33,39 +47,43 @@ public class UserLoginApplication {
 			}
 
 			boolean loginSuccessful = false;
+
 			int failedAttempts = 0;
-			while (true) {
-				failedAttempts++;
-				System.out.println("Enter your email");
-				String username = scan.nextLine();
-				System.out.println("Enter your password");
-				String password = scan.nextLine();
 
-				for (User loginInfo : userArray) {
+			while (loginSuccessful == false) {
 
-					if (loginInfo.getUsername().equalsIgnoreCase(username)
-							&& loginInfo.getPassword().equals(password)) {
-						System.out.println("Welcome: " + loginInfo.getName());
-						loginSuccessful = true;
+				while (true) {
+					failedAttempts++;
+					System.out.println("Enter your email");
+					String username = scan.nextLine();
+					System.out.println("Enter your password");
+					String password = scan.nextLine();
+
+					for (User loginInfo : userArray) {
+
+						if (loginInfo.getUsername().equalsIgnoreCase(username)
+								&& loginInfo.getPassword().equals(password)) {
+							System.out.println("Welcome: " + loginInfo.getName());
+							loginSuccessful = true;
+							break;
+						} else if (!loginInfo.getUsername().equalsIgnoreCase(username)
+								|| !loginInfo.getPassword().equals(password)) {
+
+							loginSuccessful = false;
+
+						}
+						if (failedAttempts == 4) {
+							System.out.println("Too many failed login attempts, you are now locked out.");
+							loginSuccessful = true;
+							break;
+						}
+					}
+					if (loginSuccessful == true) {
 						break;
-					}
-
-					else if (!loginInfo.getUsername().equalsIgnoreCase(username)
-							|| !loginInfo.getPassword().equals(password)) {
-
-						loginSuccessful = false;
+					} else {
+						System.out.println("Invalid login, please try again");
 
 					}
-					if (failedAttempts == 5) {
-						System.out.println("Too many failed login attempts, you are now locked out.");
-
-						break;
-					}
-				}
-				if (loginSuccessful == true) {
-					break;
-				} else if (loginSuccessful == false) {
-					System.out.println("Invalid login, please try again");
 				}
 			}
 		}
